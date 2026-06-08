3c40305d1c2cbc66d4f57ade8c8a853d544e54c1
fl4v1u-_- <flaviumoldovan208@gmail.com>
2026-06-08 04:32:41 +0200

    Add files via upload

---
+++ web-crawler--main/web-scraper/src/main/java/com/zenrows/Product.java
@@ -0,0 +1,51 @@
+package com.zenrows;
+
+public class Product {
+
+    private String url;
+    private String image;
+    private String name;
+    private String price;
+
+    // Getter und Setter
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "{ \"url\":\"" + url + "\", "
+                + "\"image\":\"" + image + "\", "
+                + "\"name\":\"" + name + "\", "
+                + "\"price\":\"" + price + "\" }";
+    }
+}
